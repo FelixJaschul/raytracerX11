d5da1d888918974a4cae8d464f1e22f19f0bcbb4
felixjaschul <felix@jaschul.de>
2026-01-04 19:09:44 +0100

    .

--- main.c
+++ main.c
@@ -161,7 +161,7 @@ Vec3 calculate_ray_color(const RAY ray, const int depth)
 
 void tonemap(Vec3 *color)
 {
-#define TONEMAP_ACES
+#define TONEMAP_ACESw
 #ifdef  TONEMAP_ACES
     const float a = 2.51f;
     const float b = 0.03f;
